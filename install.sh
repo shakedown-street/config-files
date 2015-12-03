70ea1c01142ae8163fcb3a9d113c354f41bf4311
jprice <jprice@izeni.com>
2015-12-03 15:52:19 -0700

    openbox fixes

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # simple script to install all the files into the correct directories
 cp -R ./home/dot_config/openbox/* ~/.config/openbox
-cp -R ./home/dot_config/tint2/* ~/.config/
+cp -R ./home/dot_config/tint2/* ~/.config/tint2
 cp -R ./home/dot_i3/* ~/.i3/
 cp -R ./home/dot_themes/* ~/.themes/
 cp ./home/.Xresources ~/.Xresources
