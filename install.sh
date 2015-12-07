b32a0d72830014adc7ac829a7a744a615a03dac7
A-Sharp-C <pricejordan@outlook.com>
2015-12-06 20:27:47 -0700

    Added icons and added them to the installation script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 cp -R ./home/dot_config/openbox/* ~/.config/openbox
 cp -R ./home/dot_config/tint2/* ~/.config/tint2
 cp -R ./home/dot_i3/* ~/.i3/
+cp -R ./home/dot_icons/* ~/.icons/
 cp -R ./home/dot_themes/* ~/.themes/
 cp ./home/.Xresources ~/.Xresources
 
