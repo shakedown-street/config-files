2dcdf1ba5382cc0d1635982c47bfdaa5b6044f28
jprice <jprice@izeni.com>
2016-02-23 12:29:40 -0700

    I3 with compton and i3-gaps

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # simple script to install all the files into the correct directories
 cp -R ./home/dot_config/openbox/* ~/.config/openbox
 cp -R ./home/dot_config/tint2/* ~/.config/tint2
+cp ./home/dot_config/compton.conf ~/.config/compton.conf
 cp -R ./home/dot_i3/* ~/.i3/
 cp -R ./home/dot_themes/* ~/.themes/
 cp ./home/.Xresources ~/.Xresources
