fa113e929479ff134cd5f0d4f0aa0ffdcd024b96
jprice <jprice@izeni.com>
2021-10-01 11:13:58 -0600

    Update configs to match new desktop

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cp -R ./home/dot_config/openbox/* ~/.config/openbox
 cp -R ./home/dot_config/tint2/* ~/.config/tint2
 cp ./home/dot_config/compton.conf ~/.config/compton.conf
-cp -R ./home/dot_i3/* ~/.i3/
+cp -R ./home/dot_i3/* ~/.config/i3/
 cp -R ./home/dot_themes/* ~/.themes/
 cp -R ./home/dot_vim/* ~/.vim/
 cp ./home/.Xresources ~/.Xresources
@@ -16,6 +16,6 @@ xrdb ~/.Xresources
 
 # If using .vim/ and .vimrc, clone vundle, run :PluginInstall in vim and then run
 # the command to make vimproc
-# 
+#
 # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # cd ~/.vim/bundle/vimproc.vim && make
